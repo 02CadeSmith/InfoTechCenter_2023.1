4aa3c82361ddf8bbe2c5721e41eadb0e9d5f51cb
SmithC <02Cade.Smith@gulllakecs.org>
2023-11-01 07:58:16 -0400

    Add list (GasStations) function - Called and Stable

--- ProjectOrangutan.py
+++ ProjectOrangutan.py
@@ -11,11 +11,20 @@ def gasLevelGauge():
     CurrentGasLevel = random.choice(GasLevelList)
     return CurrentGasLevel
 
+# Function with a list of Gas Stations
+def listOfGasStation():
+    gasStations = ["Shell", " Speedway", "Exxon", "Meijer", "Costco", "Marathon", "BP", "Circle K", " Wesco"]
+    gasStationsNearby = random.choice(gasStations)
+    return gasStationsNearby
+
+
+
 # Function will call the gas level gauge to determine gas level and then find a close gas station if low
 def gasLevelAlert():
     milesToGasStationsLow = round(random.uniform(1, 25,), 1)
     milesToGasStationQuarterTank = round(random.uniform(25.1, 50, ), 1)
     print("low =", milesToGasStationsLow)
     print("quarter tank =", milesToGasStationQuarterTank)
-gasLevelAlert()
 
+
+gasLevelAlert()
