f7b7ab668789783c6912487ee45c080ce41d320c
SmithC <02Cade.Smith@gulllakecs.org>
2023-11-03 08:16:57 -0400

    Added Libraries and the weather functions  Tested & stable

---
+++ ProjectOrangutan.py
@@ -0,0 +1,14 @@
+print("\n****************************\n")
+
+print("weather Branch")
+
+# Import Libraries here
+import random
+from time import sleep
+
+# Create a function randomly choosing the weather from a list
+def weather():
+    weatherForecast = ["Snowing", "Blizzard", "Rain", "Foggy", "Windy", "Icy", "Sunny", "Cloudy"]
+    weatherCondition = random.choice(weatherForecast)
+     return weatherCondition
+    
